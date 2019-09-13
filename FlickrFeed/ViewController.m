9931051a566bfdfc514d4aa91857f8e97e2697d2
Angel Alberto Rueda Mejia <angel.rueda@hotmail.com>
2019-09-13 00:03:41 -0700

    Added refresh control

--- FlickrFeed/ViewController.m
+++ FlickrFeed/ViewController.m
@@ -15,6 +15,9 @@
 
 @property UIPageViewController* pageController;
 @property NSArray* items;
+@property IBOutlet UIView* activityIndicatorView;
+@property FlickrFeed* feed;
+@property CAShapeLayer* shapeLayer;
 
 /**
  Gets a FlickrFeedItemViewController based on the given index
@@ -22,7 +25,19 @@
  @param pageIndex the index of the page to retrieve.
  */
 - (FlickrFeedItemViewController *)viewControllerWithPageIndex:(NSInteger)pageIndex;
+/**
+ Creates a UIPageViewController and loads it as a childViewController
+ 
+ This method runs on the main thread.
+ */
 - (void) attachPageViewController;
+/**
+ Triggers a Flickr feed refresh
+ */
+- (void) refresh;
+/**
+ */
+- (void) addReloadControl;
 
 @end
 
@@ -31,17 +46,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Do any additional setup after loading the view.
+    self.activityIndicatorView.layer.cornerRadius = 5;
+    self.activityIndicatorView.layer.masksToBounds = true;
     
-    FlickrFeed* feed = [[FlickrFeed alloc] initWithString:@"https://api.flickr.com/"];
+    [self addReloadControl];
     
-    [feed refreshWithHandler:^(NSArray *items) {
+    self.feed = [[FlickrFeed alloc] initWithString:@"https://api.flickr.com/"];
+    
+    [self refresh];
+}
+
+- (void) addReloadControl {
+    
+    self.shapeLayer = [[CAShapeLayer alloc] init];
+    self.shapeLayer.frame = CGRectMake(0, 0, self.view.bounds.size.width, 80);
+    self.shapeLayer.backgroundColor = [[UIColor colorWithRed:57/255.0
+                                                       green:67/255.0
+                                                        blue:89/255.0
+                                                       alpha:1.0] CGColor];
+    [self.view.layer addSublayer:self.shapeLayer];
+    
+    UIPanGestureRecognizer* gestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self
+                                                                                        action:@selector(panGestureDidMove:)];
+    [self.view addGestureRecognizer:gestureRecognizer];
+    
+}
+
+- (void) panGestureDidMove:(UIPanGestureRecognizer*) gesture {
+    
+    CGRect temp = self.shapeLayer.frame;
+    
+    if(gesture.state == UIGestureRecognizerStateEnded ||
+       gesture.state == UIGestureRecognizerStateFailed ||
+       gesture.state == UIGestureRecognizerStateCancelled) {
+        temp.size.height = 80;
+    } else {
+        temp.size.height = 80 + MAX([gesture translationInView:self.view].y, 0);
+    }
+    self.shapeLayer.frame = temp;
+    
+}
+     
+     
+- (void) refresh {
+    
+    [self.feed refreshWithHandler:^(NSArray *items) {
         
         self.items = items;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.activityIndicatorView.hidden = YES;
+        });
+        
         [self attachPageViewController];
         
     }];
+    
 }
+    
 
 - (FlickrFeedItemViewController *)viewControllerWithPageIndex:(NSInteger)pageIndex {
     if (pageIndex < 0 || pageIndex >= [self.items count]) {
@@ -67,7 +129,7 @@
         self.pageController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
         
         self.pageController.dataSource = self;
-        [[self.pageController view] setFrame:[[self view] bounds]];
+        [[self.pageController view] setFrame:CGRectMake(0, 150, self.view.bounds.size.width, self.view.bounds.size.height-150)];
         
         UIViewController *initialViewController = [self viewControllerWithPageIndex:0];
         
